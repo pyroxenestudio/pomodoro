116a2d6b5d0341cdfebcfbe188659a99665952cf
Alejandro Barata <alexbgdev@gmail.com>
2025-04-01 19:28:58 +0200

    - Feature: Create About, changelog and webpage in the footer - Fix: dark mode for the modal

--- src/components/groups/footer.tsx
+++ src/components/groups/footer.tsx
@@ -1,7 +1,25 @@
+import Button from "../elements/button";
+import Modal from "../modal";
+
 export function Footer() {
   return (
-    <footer className='h-12 flex justify-center items-center'>
-      This is a footer
+    <footer className='h-12 flex justify-start items-center'>
+      <Modal title='About' icon={<span>About</span>} buttonStyles='p-2'>
+        <section>
+          This is an open source pomodoro
+          <h2 className='text-2xl'>Features</h2>
+          <ul className=''>
+            <li>Dark / Light Mode</li>
+            <li>Set your time (Pomodoro, Break, Long Break)</li>
+            <li>Working in the background using minimal resources</li>
+          </ul>
+        </section>
+      </Modal>
+      <Modal title='Changelog' icon={<span>Changelog</span>} buttonStyles='p-2'>
+        <section>
+        </section>
+      </Modal>
+      <Button variant='transparent' className='!font-normal'><a href='#'>Pyroxene</a></Button>
     </footer>
   )
 }
